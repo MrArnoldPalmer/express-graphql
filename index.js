5d97069ccaa32e363f78936646ebe18e77a4c799
Mitchell Valine <mitchell.valine@mirumagency.com>
2015-11-28 20:17:53 -0800

    index.js file imports app from app.js and starts server listening on env var PORT or 3000

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Application entry point
+import app from './app';
+import debug from 'debug';
+
+app.set('port', process.env.PORT || 3000);
+
+const server = app.listen(app.get('port'), () => {
+  debug.log(`Express server listening on port ${server.address().port}`);
+});
