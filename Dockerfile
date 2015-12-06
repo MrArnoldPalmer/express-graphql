f3e50f6d3a74b22c4f50397e17188d7391dddcdd
Mitchell Valine <mitchell.valine@mirumagency.com>
2015-12-06 11:24:17 -0800

    Schema kind of set up? need mutations and shit

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
-FROM node:latest
+Fplayerctl previousROM node:latest
 ADD . /kanban-api
 WORKDIR /kanban-api
 RUN npm install
 RUN npm install nodemon -g
+RUN npm install babel-cli -g
 CMD npm start
--- queries/user.js
+++ queries/user.js
@@ -1,33 +1,64 @@
-import {GraphQLSchema, GraphQLObjectType, GraphQLInt} from 'graphql';
+import {GraphQLSchema, GraphQLObjectType, GraphQLInt, GraphQLNonNull, GraphQLString, GraphQLList} from 'graphql';
+import co from 'co';
 import User from '../schema/user';
 
-let count = 0;
+function getProjection(fieldASTs) {
+  return fieldASTs.selectionSet.selections.reduce((projections, selection) => {
+    projections[selection.name.value] = 1;
+    return projections;
+  }, {});
+}
 
-let schema  =  new GraphQLSchema({
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  description: 'User Creator',
+  fields: () => ({
+    id: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: 'User ID'
+    },
+    userName: {
+      type: GraphQLString,
+      description: 'User Name'
+    },
+    repos: {
+      type: new GraphQLList(UserType),
+      description: 'Repos owned by user, or empty list if none',
+      resolve: (user, params, source, fieldASTs) => {
+        let projections = getProjection(fieldASTs);
+        return User.find({
+          _id: {
+            $in: user.repos.map((id) => id.toString())
+          }
+        }, projections);
+      }
+    }
+  })
+});
+
+const Schema = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
-      count: {
-        type: GraphQLInt,
-        resolve: () =>  {
-          return count;
-        }
-      }
-    }
-  }),
-  mutation: new GraphQLObjectType({
-    name: 'RootMutationType',
-    fields: {
-      updateCount: {
-        type: GraphQLInt,
-        description: 'Updates the count',
-        resolve: function() {
-          count += 1;
-          return count;
+      hello: {
+        type: GraphQLString,
+        resolve: () => 'World'
+      },
+      user: {
+        type: UserType,
+        args: {
+          id: {
+            name: 'id',
+            type: new GraphQLNonNull(GraphQLString)
+          }
+        },
+        resolve: (root, {id}, source, fieldASTs) => {
+          let projections =  getProjection(fieldASTs);
+          return User.findById(id, projections);
         }
       }
     }
   })
 });
 
-export default schema;
+export default Schema;
